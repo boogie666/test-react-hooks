3b7ac3183339b8dfe1c631950c712be2aaa43f7f
boogie666 <bbugarschi@gmail.com>
2018-11-24 17:31:57 +0200

    better hooks

--- src/test_react_hooks/core.cljs
+++ src/test_react_hooks/core.cljs
@@ -17,8 +17,8 @@
         (vswap! history pop)
         (set-value current))]))
 
-(defn use-window-title [atom value]
-  (h/use-watch atom #(set! (.-title js/document) (value %))))
+(defn use-window-title [atom]
+  (h/use-watch atom #(set! (.-title js/document) %)))
 
 (defn counter-reducer [state action]
   (case action
@@ -27,7 +27,7 @@
 
 (defn counter []
   (let [[val set-val undo] (use-undo 0)
-        _ (use-window-title val identity)]
+        _ (use-window-title val)]
     (fn []
       [:div {:style {:margin 10}}
        [:button {:on-click #(set-val (inc @val))} "+"]
@@ -36,24 +36,25 @@
        [:button {:on-click undo} "Undo"]])))
 
 
+(defn trans [x y]
+  (str "translate(" x "px, " y "px)"))
+
 (defn mouse-tracker [offset]
   (let [[x, set-x] (h/use-state 0)
         [y, set-y] (h/use-state 0)
-        div (h/use-ref)
         _ (h/use-effect (fn []
                           (let [handler #(let [x (-> % .-clientX)
                                                y (-> % .-clientY)]
                                            (set-x x)
-                                           (set-y y)
-                                           (when-let [d @div]
-                                             (set! (.-transform (.-style d))
-                                                   (str "translate(" (+ offset x) "px, " (+ offset y) "px)"))))]
+                                           (set-y y))]
                             (.addEventListener js/document "mousemove" handler)
                             #(.removeEventListener js/document "mousemove" handler))))]
     (fn []
-      [:div {:ref div :style {:position "absolute"
-                              :transform "translate(0px, 0px)"}}
-       (str @x "px " @y "px")])))
+      [:div {:style {:position "absolute"
+                     :background-color "#ff00ff"
+                     :width 10
+                     :height 10
+                     :transform (trans (+ @x offset) (+ @y offset))}}])))
 
 
 (defn counters-reducers [state action]
@@ -62,8 +63,7 @@
     :remove (update state :counters dec)))
 
 (defn hello-world []
-  (let [[state dispatch] (h/use-reducer counters-reducers {:counters 3})
-        _ (use-window-title state :counters)]
+  (let [[state dispatch] (h/use-reducer counters-reducers {:counters 3})]
     (fn []
       [:div
        [mouse-tracker 10]
@@ -78,7 +78,4 @@
                     (. js/document (getElementById "app")))
 
 (defn on-js-reload [])
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
 
--- src/test_react_hooks/hooks.cljs
+++ src/test_react_hooks/hooks.cljs
@@ -6,7 +6,6 @@
   (let [r (r/atom value)]
     [r #(reset! r %)]))
 
-
 (defn use-ref []
   (let [a (volatile! nil)]
     (reify
@@ -17,6 +16,15 @@
       (-invoke [this value]
         (vreset! a value)))))
 
+(defn use-update [f]
+  (let [current-component (r/current-component)
+        did-update (.-componentDidUpdate current-component)]
+
+    (set! (.-componentDidUpdate current-component)
+          (fn [& args]
+            (f)
+            (when (fn? did-update)
+              (apply did-update args))))))
 
 
 (defn use-effect [f]
@@ -25,25 +33,27 @@
         will-unmount (.-componentWillUnmount current-component)
         handler (volatile! nil)]
 
-    (set! (.-componentDidMount current-component) (fn [& args]
-                                                    (vreset! handler (f))
-                                                    (apply did-mount args)))
-    (set! (.-componentWillUnmount current-component) (fn [& args]
-                                                       (when-let [h @handler] (h))
-                                                       (apply will-unmount args)))))
+    (set! (.-componentDidMount current-component)
+          (fn [& args]
+            (let [h (f)]
+              (when (fn? h)
+                (vreset! handler h)))
+            (when (fn? did-mount)
+              (apply did-mount args))))
 
+    (set! (.-componentWillUnmount current-component)
+          (fn [& args]
+            (when (fn? @handler) (@handler))
+            (when (fn? will-unmount)
+              (apply will-unmount args))))))
 
 (defn use-watch [atom f]
   (use-effect (fn []
-                (f @atom)
-                (add-watch atom ::ref-watcher
-                            (fn [_ _ _ new-val]
-                              (f new-val)))
-                #(remove-watch atom ::ref-watcher))))
-
-
-
+                (let [ref-watcher (keyword (gensym "ref-counter"))]
+                  (add-watch atom ref-watcher #(f %4))
+                  #(remove-watch atom ref-watcher)))))
 
 (defn use-reducer [reducer initial-state]
   (let [val (r/atom initial-state)]
     [val #(swap! val reducer %)]))
+
